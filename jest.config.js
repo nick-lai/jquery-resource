45e274b73b258f9aa63c39077e463375269507d8
Nick Lai <resxc13579@gmail.com>
2021-06-04 21:31:26 +0800

    chore(deps-dev): bump jest from 26.6.3 to 27.0.4 (#17)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
   testRegex: [
     "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$"
   ],
